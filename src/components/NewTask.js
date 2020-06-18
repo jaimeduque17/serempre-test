d14d774d2967dec388d55a3c7fe16752c8e321a1
Jaime Duque <jaime.duque@estratek.com.co>
2020-06-18 11:51:18 -0500

    the form to create a new task is created

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,9 +1,60 @@
 import React from 'react';
 
 const NewTask = () => {
-    return (  
-        <h1>Nueva Tarea</h1>
+    return (
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Agregar Nueva Tarea
+                        </h2>
+                        <form>
+                            <div className="form-group">
+                                <label>Nombre Tarea</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre Tarea"
+                                    name="name"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Descripción Tarea</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Descripción Tarea"
+                                    name="description"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Tiempo Estimado Tarea</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Descripción Tarea"
+                                    name="estimated"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Tiempo Trabajado Tarea</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Descripción Tarea"
+                                    name="worked"
+                                />
+                            </div>
+                            <button type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >Agregar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
- 
+
 export default NewTask;
