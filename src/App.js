434144172d9788b2310dddcbe1769360a6aa9b8f
Jaime Duque <jaime.duque@estratek.com.co>
2020-06-18 14:27:25 -0500

    redux is configured

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import Tasks from './components/Tasks';
 import NewTask from './components/NewTask';
 import EditTask from './components/EditTask';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="container mt-5">
-        <Switch>
-          <Route exact path="/" component={Tasks} />
-          <Route exact path="/tasks/new" component={NewTask} />
-          <Route exact path="/tasks/edit/:id" component={EditTask} />
-        </Switch>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={Tasks} />
+            <Route exact path="/tasks/new" component={NewTask} />
+            <Route exact path="/tasks/edit/:id" component={EditTask} />
+          </Switch>
+        </div>
+      </Provider>
     </Router>
   );
 }
---
+++ src/reducers/tasksReducer.js
@@ -0,0 +1,13 @@
+// each reducer has its own state
+const initialState = {
+    tasks: [],
+    error: null,
+    loading: false
+}
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
