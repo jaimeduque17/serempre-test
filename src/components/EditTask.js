351f5bd1fdce93c4fea4f2c28ac3fd28a260e90c
Jaime Duque <jaime.duque@estratek.com.co>
2020-06-18 12:01:20 -0500

    components updated

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -2,7 +2,58 @@ import React from 'react';
 
 const EditTask = () => {
     return (  
-        <h1>Edit Task</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Editar Tarea
+                        </h2>
+                        <form>
+                            <div className="form-group">
+                                <label>Nombre Tarea</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre Tarea"
+                                    name="name"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Descripción Tarea</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Descripción Tarea"
+                                    name="description"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Tiempo Estimado Tarea</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Descripción Tarea"
+                                    name="estimated"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Tiempo Trabajado Tarea</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Descripción Tarea"
+                                    name="worked"
+                                />
+                            </div>
+                            <button type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >Guardar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
  
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 const Tasks = () => {
     return (  
-        <h1>Tareas</h1>
+        <>
+            <h2 className="text-center my-5">Listado de Tareas</h2>
+            <table className="table table-striped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Tiempo Trabajado</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </>
     );
 }
  
